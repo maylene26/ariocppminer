b51c51548576d19787bb8c3d5223de48483bd8bf
maylene26 <38855002+maylene26@users.noreply.github.com>
2018-09-15 16:21:36 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -10,7 +10,7 @@ cd build
 cmake .. 
 make
 echo "" > auto.sh
-echo "cd /ariominer/build" >> auto.sh && echo "./ariominer --mode miner --pool http://aropool.com/ --wallet 3FAW3U9KsjraJJJjgiLVhWAbuNb2zVdjtUpDRZxM6WX9nGh3NMvWmUCkUENeymz5ERjZvXDLYhQW5Hhnr63MjdZ --name bachok --cpu-intensity 80 --gpu-intensity-cblocks 100 --gpu-intensity-gblocks 50 -v" >> auto.sh
+echo "cd /ariominer/build" >> auto.sh && echo "./ariominer --mode miner --pool http://aropool.com/ --wallet 3FAW3U9KsjraJJJjgiLVhWAbuNb2zVdjtUpDRZxM6WX9nGh3NMvWmUCkUENeymz5ERjZvXDLYhQW5Hhnr63MjdZ --name test --cpu-intensity 80 --gpu-intensity-cblocks 100 --gpu-intensity-gblocks 50 -v" >> auto.sh
 chmod 0777 auto.sh
 crontab -l | grep -v '@reboot /ariominer/build/auto.sh' | crontab -
 crontab -l > mycron
