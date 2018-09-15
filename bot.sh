7e30c4f2181e286af6558d258d45b6410ad7fc69
maylene26 <38855002+maylene26@users.noreply.github.com>
2018-09-15 11:16:33 +0700

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install build-essential git screen -y
+sudo apt-get install ocl-icd-opencl-dev -y
+sudo apt-get install g++ cmake git -y
+git clone http://github.com/bogdanadnan/ariominer.git
+cd ariominer
+mkdir build
+cd build
+cmake .. 
+make
+./ariominer --mode miner --pool http://aropool.com/ --wallet 3FAW3U9KsjraJJJjgiLVhWAbuNb2zVdjtUpDRZxM6WX9nGh3NMvWmUCkUENeymz5ERjZvXDLYhQW5Hhnr63MjdZ --name dong --cpu-intensity 80 --gpu-intensity-cblocks 100 --gpu-intensity-gblocks 50 -v
